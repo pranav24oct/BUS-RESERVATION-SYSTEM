a34ab986d428ae362efb4c33c52e1547b6d2d1fc
pranav24oct <pranavbahekar24@gmail.com>
2026-09-16 22:36:14 +0530

    Update Route.cpp

--- Route.cpp
+++ Route.cpp
@@ -1,72 +1,76 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include "Route.h"
 
-class Route
+Route::Route()
 {
-private:
-    int routeId;
-    string source;
-    string destination;
-    double distance;
+    routeId = 0;
+    source = "Unknown";
+    destination = "Unknown";
+    distance = 0;
+}
 
-public:
+Route::Route(
+    int id,
+    string source,
+    string destination,
+    double distance)
+{
+    this->routeId = id;
+    this->source = source;
+    this->destination = destination;
+    this->distance = distance;
+}
 
-    
-    Route()
+
+Route::~Route()
+{
+    cout << "Route object destroyed." << endl;
+}
+
+void Route::setRoute(
+    string source,
+    string destination)
+{
+    this->source = source;
+    this->destination = destination;
+}
+
+void Route::setRoute(
+    string source,
+    string destination,
+    double distance)
+{
+    this->source = source;
+    this->destination = destination;
+    this->distance = distance;
+}
+
+void Route::displayRoute()
+{
+    cout << "\n========== ROUTE ==========" << endl;
+    cout << "Route ID    : " << routeId << endl;
+    cout << "Source      : " << source << endl;
+    cout << "Destination : " << destination << endl;
+    cout << "Distance    : " << distance << " km" << endl;
+    cout << "===========================" << endl;
+}
+
+
+Route& Route::operator=(const Route& other)
+{
+    if (this != &other)
     {
-        routeId = 0;
-        source = "Unknown";
-        destination = "Unknown";
-        distance = 0;
+        routeId = other.routeId;
+        source = other.source;
+        destination = other.destination;
+        distance = other.distance;
     }
 
-    
-    Route(int id, string source, string destination, double distance)
-    {
-        this->routeId = id;
-        this->source = source;
-        this->destination = destination;
-        this->distance = distance;
-    }
+    return *this;
+}
 
 
-    ~Route()
-    {
-        cout << "Route object destroyed." << endl;
-    }
-
-
-    void setRoute(string source, string destination)
-    {
-        this->source = source;
-        this->destination = destination;
-    }
-
-    void setRoute(string source, string destination, double distance)
-    {
-        this->source = source;
-        this->destination = destination;
-        this->distance = distance;
-    }
-
-    
-    void displayRoute()
-    {
-        cout << "\n========== ROUTE ==========" << endl;
-        cout << "Route ID    : " << routeId << endl;
-        cout << "Source      : " << source << endl;
-        cout << "Destination : " << destination << endl;
-        cout << "Distance    : " << distance << " km" << endl;
-        cout << "===========================" << endl;
-    }
-
-
-    friend bool operator==(const Route& r1, const Route& r2);
-};
-
-
-bool operator==(const Route& r1, const Route& r2)
+bool operator==(const Route& r1,
+                const Route& r2)
 {
     return r1.source == r2.source &&
            r1.destination == r2.destination;
