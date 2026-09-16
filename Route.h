c3e40a4945f7ca03a2d5ff4f08a7ddb2935124f0
pranav24oct <pranavbahekar24@gmail.com>
2026-09-16 22:26:17 +0530

    Add Route class with route details and methods

---
+++ Route.h
@@ -0,0 +1,24 @@
+class Route
+{
+private:
+    int routeId;
+    string source;
+    string destination;
+    double distance;
+
+public:
+    Route();
+    Route(int id, string source, string destination, double distance);
+    ~Route();
+
+    void setRoute(string source, string destination);
+    void setRoute(string source, string destination, double distance);
+
+    void displayRoute();
+
+    // Assignment operator
+    Route& operator=(const Route& other);
+
+    // Friend operator
+    friend bool operator==(const Route& r1, const Route& r2);
+};
